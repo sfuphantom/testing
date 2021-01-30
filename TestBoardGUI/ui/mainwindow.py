44a733e844803dccfe0b0842b6f5fdd03eee9506
tnookut <nookuttanya@gmail.com>
2021-01-29 18:21:57 -0800

    revise gui design and front end code to populate QTreeView from json file

--- TestBoardGUI/ui/mainwindow.py
+++ TestBoardGUI/ui/mainwindow.py
@@ -1,150 +1,142 @@
 import sys
 import os
+import json
 from datetime import date
 
+from PySide2.QtWidgets import (QApplication, QPushButton, QLineEdit, QTabWidget,
+                               QTreeWidget, QComboBox, QTreeWidget, QTableWidget,
+                               QWidget, QTreeWidgetItem, QHeaderView)
+from PySide2.QtCore import (QFile, QObject, QDir, Signal, Slot)
+from PySide2.QtUiTools import (QUiLoader)
 
-from PySide2.QtWidgets import (QApplication, QMainWindow, QWidget, QPushButton,
-                               QTabWidget, QTreeWidget, QLineEdit, QComboBox,
-                               QTreeWidgetItem, QTextEdit, QHeaderView)
-from PySide2.QtCore import QFile, QObject, QDir, Signal, Slot
-from PySide2.QtUiTools import QUiLoader
-
-
+# Set-up/Connect mainwindow
 class MainWindow(QObject):
+    
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
-        self.checkin_info = {'date': '', 'board_name': '', 'team_member': '',
-                             'device_type': '', 'testID': '',
-                             'board_version': ''}
-
-        self.result_temp = {'category': '', 'test_name': '', 'outcome': ''}
+        # Check-In Information
+        self.checkin_info = {'device': '', 'board_name': '',
+                             'board_version': '', 'testID': '',
+                             'team_member': '', 'date': str(date.today())}
 
         # Load ui file
         loader = QUiLoader()
-        path = os.path.join(os.path.dirname(__file__), "mainwindow.ui")
+        path = os.path.join(os.path.dirname(__file__), 'mainwindow.ui')
         ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly | QFile.ExistingOnly)
-        self.ui_widget = loader.load(ui_file)
+        ui_file.open(QFile.ReadOnly)
+        self.mainwindow = loader.load(ui_file)
         ui_file.close()
 
-        # Connecting python to ui features
-        self.mainscreen = self.ui_widget.findChild(QWidget, "centralwidget")
-
-        # LineEdits
-        self.date = self.ui_widget.findChild(QLineEdit, "Date")
-        self.date.setText(str(date.today()))
-        self.date.setReadOnly(True)
-        self.board_name = self.ui_widget.findChild(QLineEdit, "BoardName")
-        self.team_member = self.ui_widget.findChild(QLineEdit, "TeamMember")
-        self.testID = self.ui_widget.findChild(QLineEdit, "TestID")
-        self.board_version = self.ui_widget.findChild(QLineEdit, "version")
+        # Connect Python to ui features
 
         # ComboBox
-        self.device = self.ui_widget.findChild(QComboBox, "Device")
-        self.device.currentTextChanged.connect(self._device_selected)
+        self.device = self.mainwindow.findChild(QComboBox, 'deviceSelection')
+        self.device.currentTextChanged.connect(lambda: self._device_selected(
+                                               self.device.currentIndex()))
 
-        # PushButtons
-        self.calibrationPushButton = self.ui_widget.findChild(
-                                        QPushButton, "CalibrationTest")
-        self.calibrationPushButton.clicked.connect(self.calibration_test)
-
-        self.savePushButton = self.ui_widget.findChild(QPushButton, "Save")
-        self.savePushButton.clicked.connect(self.save)
-
-        self.cancelPushButton = self.ui_widget.findChild(QPushButton, "Cancel")
-        self.cancelPushButton.clicked.connect(self.cancel)
-
-        self.runPushButton = self.ui_widget.findChild(QPushButton, "Run")
-        self.runPushButton.clicked.connect(self.run)
-
-        # TabWidgets
-        self.tabwidget = self.ui_widget.findChild(QTabWidget, "TestSelection")
-        # Disable VCU Tab
+        # LineEdits
+        self.boardName = self.mainwindow.findChild(QLineEdit, 'boardNameLineEdit')
+        self.boardVersion = self.mainwindow.findChild(QLineEdit, 'boardVersionLineEdit')
+        self.testID = self.mainwindow.findChild(QLineEdit, 'testIDLineEdit')
+        self.teamMember = self.mainwindow.findChild(QLineEdit, 'teamMemberLineEdit')
+        
+        # TabWidget
+        self.tabwidget = self.mainwindow.findChild(QTabWidget, 'deviceTabWidget')
+        # Disable VCU tab
         self.tabwidget.setTabEnabled(0, False)
-        # Disable BMS Tab
+        # Disable BMS tab
         self.tabwidget.setTabEnabled(1, False)
+        # Empty tab just to fix appearance
+        self.tabwidget.setTabVisible(2, False)
 
         # TreeWidget
-        self.vcu_tree = self.ui_widget.findChild(QTreeWidget, "VCUTree")
-        self.bms_tree = self.ui_widget.findChild(QTreeWidget, "BMSTree")
+        self.VCUTree = self.mainwindow.findChild(QTreeWidget, 'VCUTreeWidget')
+        self.BMSTree = self.mainwindow.findChild(QTreeWidget, 'BMSTreeWidget')
 
-        # TextEdit
-        self.resultsTextEdit = self.ui_widget.findChild(
-                                QTextEdit, 'ResultsTextEdit')
-        self.resultsTextEdit.setReadOnly(True)
+        # TableWidget
+        self.resultsTable = self.mainwindow.findChild(QTableWidget, 'resultsTable')
+        
+        # PushButtons
+        self.calibrationTestButton = self.mainwindow.findChild(
+                                        QPushButton, 'calibrationTestButton')
+        self.calibrationTestButton.clicked.connect(self.calibration)
+        self.runPushButton = self.mainwindow.findChild(QPushButton, 'runPushButton')
+        self.runPushButton.clicked.connect(self.run)
+        self.cancelPushButton = self.mainwindow.findChild(QPushButton, 'cancelPushButton')
+        self.cancelPushButton.clicked.connect(self.cancel)
+        self.savePushButton = self.mainwindow.findChild(QPushButton, 'savePushButton')
+        self.savePushButton.clicked.connect(self.save)
 
-        # Must have this for ui to appear
-        self.ui_widget.show()
+        # Show ui when application runs
+        self.mainwindow.show()
 
-    # WIP
-    def _get_selected_tests(self):
-        self.selected_tests = self.selected_tree.selectedItems()  # Returns a list of QTreeWidgetItems:
-        print(self.selected_tests)
-        # for test_case in self.selected_tests:
-        #     print(test_case.text(0))
-        #     if test_case.parent():
-        #         print(test_case.parent().text(0))
+    # Populate test cases in test selection tree
+    def _fill_test_cases(self, device: str):
+        # Select tree to populate base on selected device
+        if device == 'VCU':
+            tree = self.VCUTree
+        elif device == 'BMS':
+            tree = self.BMSTree
 
-    def _write_textedit(self, text):
-        self.resultsTextEdit.append(text)
+        # Load testcases JSON
+        path = os.path.join(os.path.dirname(__file__), 'testcases.json')
+        with open(path) as f:
+            test_cases = json.load(f)
 
+        # Populate QTreeWidget
+        for thing in test_cases[device]:
+            for key, value in thing.items():
+                root = QTreeWidgetItem(tree, [key])
+                for val in value:
+                    item = QTreeWidgetItem(root, list(val.values()))
+        
+    # Retrieve checkin information
     def _get_checkin_info(self):
-        self.checkin_info['date'] = self.date.text()
-        self.checkin_info['board_name'] = self.board_name.text()
-        self.checkin_info['team_member'] = self.team_member.text()
-        self.checkin_info['testID'] = self.board_name.text()
-        self.checkin_info['board_version'] = self.board_name.text()
-        self.checkin_info['device_type'] = self.device.currentText()
+        self.checkin_info['device'] = self.device.currentText()
+        self.checkin_info['board_name'] = self.boardName.text()
+        self.checkin_info['board_version'] = self.boardVersion.text()
+        self.checkin_info['testID'] = self.testID.text()
+        self.checkin_info['team_member'] = self.teamMember.text()
 
-    @Slot()
-    def _device_selected(self):
+        print(self.checkin_info)
+    
+    # Enable test selection tab according to selected device
+    def _device_selected(self, index: int):
         # Disable both tabs
         self.tabwidget.setTabEnabled(0, False)
         self.tabwidget.setTabEnabled(1, False)
-        # Identify selected device
-        device = self.device.currentText()
-        # Enable tab corresponding to selected device
-        if device == "VCU":
-            self.tabwidget.setCurrentIndex(0)
-            self.tabwidget.setTabEnabled(0, True)
-            self.selected_tree = self.vcu_tree
-        elif device == "BMS":
-            self.tabwidget.setCurrentIndex(1)
-            self.tabwidget.setTabEnabled(1, True)
-            self.selected_tree = self.bms_tree
 
+        # Switch to selected device tab
+        self.tabwidget.setCurrentIndex(index)
+        self.tabwidget.setTabEnabled(index, True)
+
+        # Populate test selection
+        self._fill_test_cases(self.device.currentText())
+
+    # Run tests
     @Slot()
     def run(self):
-        # Run selected tests and print results on screen
-        # Retrieve selected tests
-        self._get_selected_tests()
-        self._write_textedit('Running tests')
+        print('run button is clicked')
 
+    # Cancel tests
     @Slot()
     def cancel(self):
-        self._write_textedit('Cancel')
-        # Stop running tests
+        print('cancel button is clicked')
 
-    @Slot()
-    def calibration_test(self):
-        # Run calibration test
-        self._write_textedit('Starting calibration test...')
+    # Run calibration test
+    @Slot() 
+    def calibration(self):
+        print('calibration test button is clicked')
 
+    # Save test reults
     @Slot()
     def save(self):
-        self._write_textedit('Saving results')
         self._get_checkin_info()
-        print(self.checkin_info)
-        self._write_textedit('Test Summary:\n')
-        for key in self.checkin_info.keys():
-            self._write_textedit(key + ':    ' + self.checkin_info[key])
-        # Get test results
-        # Open folder
-        # Write excel file
+        print('save button is clicked')
 
 
-if __name__ == "__main__":
+if __name__=='__main__':
     app = QApplication(sys.argv)
     mainwindow = MainWindow()
     sys.exit(app.exec_())
