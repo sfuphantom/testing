9021adf067b5093c27de22d73ad362896fe8ba32
courtenayhuffman <courtenay.huffman@gmail.com>
2021-02-04 18:31:03 -0800

    add apps comments for different fault states

--- TestBoardFirmware/phantom_drivers/vcu/source/apps.c
+++ TestBoardFirmware/phantom_drivers/vcu/source/apps.c
@@ -13,8 +13,9 @@ enum
     APPS_IMPLAUSIBILITY,
     APPS_SHORT_CIRCUIT,
     APPS_OPEN_CIRCUIT,
-    APPS_BSE_ACTIVATED,
-    //APPS_BSE_DEACTIVATED ????
+    APPS_BSE_ACTIVATED, // APPS sensor indicates >25% activation while BSE applied
+    APPS_BSE_DEACTIVATED // after APPS & BSE applied, power returns after APPS registers <5% activation
+    APPS_OUT_OF_RANGE //values outside normal operating range, but not an open or short circuit, for more than 100ms
 };
 
 // Static function prototypes
@@ -36,6 +37,11 @@ void apps_process(uint8_t state)
         case APPS_BSE_ACTIVATED:
             apps_bse_activated();
             break;
+        case APPS_BSE_DEACTIVATED:
+            apps_bse_deactivated();
+            break;
+        case APPS_OUT_OF_RANGE:
+            apps_out_of_range();
         default: 
             normal_apps_operation();
             break;
@@ -44,25 +50,48 @@ void apps_process(uint8_t state)
 
 static void normal_apps_operation()
 {
-
+    // both APPS sensors operate between X(MIN) and X(MAX) range
+    // both APPS sensor values within 10% of each other
 }
 
 static void apps_implausibility()
 {
-
+    // both APPS sensors operate between X(MIN) and X(MAX) range
+    // APPS sensors indicate >10% difference in values 
 }
 
 static void apps_short_circuit()
 {
-
+    // one or both APPS sensors indicate short circuit (5V)
+    // both APPS sensor values within 10% of each other
 }
 
 static void apps_open_circuit()
 {
-
+    // one or both APPS sensors indicate open circuit (0V)
+    // both APPS sensor values within 10% of each other
 }
 
-static void apps_bse_activated()
+static void apps_bse_activated() // how should this one be handled? involves BSE values
 {
+    // both APPS sensors indicate >25% activation
+    // both APPS sensors operate between X(MIN) and X(MAX) range
+    // both APPS sensor values within 10% of each other
+    // BSE sensor activated
+}
 
+static void apps_bse_deactivated()
+{
+    // to be executed after apps_bse_activated()
+    // both APPS sensors indicate <5% activation
+    // both APPS sensors operate between X(MIN) and X(MAX) range
+    // both APPS sensor values within 10% of each other
+}
+}
+
+static void apps_out_of_range()
+{
+    // one or both APPS sensors indicate voltage outside normal operating range (X(MIN) - X(MAX))
+    // both APPS sensor values within 10% of each other
+}
 }
