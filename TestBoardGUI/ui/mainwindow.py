3934402e29ef38acec57cefcfa75f41156f6157c
tnookut <nookuttanya@gmail.com>
2021-02-24 10:11:28 -0800

    feat: retreive and parse user input

--- TestBoardGUI/ui/mainwindow.py
+++ TestBoardGUI/ui/mainwindow.py
@@ -3,15 +3,16 @@ import os
 import json
 from datetime import date
 
-from PySide2.QtWidgets import (QApplication, QPushButton, QLineEdit, QTabWidget,
-                               QTreeWidget, QComboBox, QTreeWidget, QTableWidget,
+from PySide2.QtWidgets import (QApplication, QPushButton, QLineEdit,
+                               QTabWidget, QTreeWidget, QComboBox, QTreeWidget,
                                QWidget, QTreeWidgetItem, QHeaderView)
-from PySide2.QtCore import (QFile, QObject, QDir, Signal, Slot)
+from PySide2.QtCore import (QFile, QObject, QDir, Signal, Slot, Qt)
 from PySide2.QtUiTools import (QUiLoader)
+
 
 # Set-up/Connect mainwindow
 class MainWindow(QObject):
-    
+
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
 
@@ -36,13 +37,17 @@ class MainWindow(QObject):
                                                self.device.currentIndex()))
 
         # LineEdits
-        self.boardName = self.mainwindow.findChild(QLineEdit, 'boardNameLineEdit')
-        self.boardVersion = self.mainwindow.findChild(QLineEdit, 'boardVersionLineEdit')
+        self.boardName = self.mainwindow.findChild(
+            QLineEdit, 'boardNameLineEdit')
+        self.boardVersion = self.mainwindow.findChild(
+            QLineEdit, 'boardVersionLineEdit')
         self.testID = self.mainwindow.findChild(QLineEdit, 'testIDLineEdit')
-        self.teamMember = self.mainwindow.findChild(QLineEdit, 'teamMemberLineEdit')
-        
+        self.teamMember = self.mainwindow.findChild(
+            QLineEdit, 'teamMemberLineEdit')
+
         # TabWidget
-        self.tabwidget = self.mainwindow.findChild(QTabWidget, 'deviceTabWidget')
+        self.tabwidget = self.mainwindow.findChild(
+            QTabWidget, 'deviceTabWidget')
         # Disable VCU tab
         self.tabwidget.setTabEnabled(0, False)
         # Disable BMS tab
@@ -53,19 +58,24 @@ class MainWindow(QObject):
         # TreeWidget
         self.VCUTree = self.mainwindow.findChild(QTreeWidget, 'VCUTreeWidget')
         self.BMSTree = self.mainwindow.findChild(QTreeWidget, 'BMSTreeWidget')
+        self.resultsTree = self.mainwindow.findChild(
+            QTreeWidget, 'resultsTreeWidget')
+        self.VCUTree.itemClicked.connect(self._duplicate_selections)
+        self.BMSTree.itemClicked.connect(self._duplicate_selections)
 
-        # TableWidget
-        self.resultsTable = self.mainwindow.findChild(QTableWidget, 'resultsTable')
-        
+
         # PushButtons
         self.calibrationTestButton = self.mainwindow.findChild(
-                                        QPushButton, 'calibrationTestButton')
+            QPushButton, 'calibrationTestButton')
         self.calibrationTestButton.clicked.connect(self.calibration)
-        self.runPushButton = self.mainwindow.findChild(QPushButton, 'runPushButton')
+        self.runPushButton = self.mainwindow.findChild(
+            QPushButton, 'runPushButton')
         self.runPushButton.clicked.connect(self.run)
-        self.cancelPushButton = self.mainwindow.findChild(QPushButton, 'cancelPushButton')
+        self.cancelPushButton = self.mainwindow.findChild(
+            QPushButton, 'cancelPushButton')
         self.cancelPushButton.clicked.connect(self.cancel)
-        self.savePushButton = self.mainwindow.findChild(QPushButton, 'savePushButton')
+        self.savePushButton = self.mainwindow.findChild(
+            QPushButton, 'savePushButton')
         self.savePushButton.clicked.connect(self.save)
 
         # Show ui when application runs
@@ -75,9 +85,9 @@ class MainWindow(QObject):
     def _fill_test_cases(self, device: str):
         # Select tree to populate base on selected device
         if device == 'VCU':
-            tree = self.VCUTree
+            self.selectedTree = self.VCUTree
         elif device == 'BMS':
-            tree = self.BMSTree
+            self.selectedTree = self.BMSTree
 
         # Load testcases JSON
         path = os.path.join(os.path.dirname(__file__), 'testcases.json')
@@ -87,10 +97,61 @@ class MainWindow(QObject):
         # Populate QTreeWidget
         for thing in test_cases[device]:
             for key, value in thing.items():
-                root = QTreeWidgetItem(tree, [key])
+                # Populate parents (test names)
+                parent = QTreeWidgetItem([key])
+                self.selectedTree.addTopLevelItems([parent])
+                # Add check box to parent
+                # parent.setCheckState(0, Qt.Unchecked)
+                # Disable selection of parents
+                parent.setFlags(parent.flags() & ~Qt.ItemIsSelectable)
+                # Populate children (test cases)
                 for val in value:
-                    item = QTreeWidgetItem(root, list(val.values()))
+                    children = QTreeWidgetItem(list(val.values()))
+                    parent.addChildren([children])
+                    # Add check box to each test case
+                    # children.setCheckState(0, Qt.Unchecked)
+    
+    # This function is buggy. WIP
+    @Slot()
+    def _duplicate_selections(self):
+        items = self.selectedTree.selectedItems()    
+        self.resultsTree.clear()
+        for item in items:
+            index = items.index(item)
+
+            parent = QTreeWidgetItem([item.parent().text(0)])
+            self.resultsTree.addTopLevelItem(parent)
+            child = QTreeWidgetItem([item.text(0)])
+            parent.addChild(child)
+
+    # Retrieve selected test cases
+    def _get_selected_tests(self):
+        # Retrieve all selected tests in -> list(QTreeWidgetItems)
+        self._rawSelection = self.selectedTree.selectedItems()
+
+        test_item = {'Test Name': '', 'Test Case': '', 'Repeat': None, 'Test Index': None}
+
+        self.selectedTests = list()
+
+        for item in self._rawSelection:
+            test_item = self._parse_tests(item)
+            self.selectedTests.append(test_item)
         
+    
+    # Parse user test selection
+    def _parse_tests(self, item):
+        test_item = {}
+        # QModelIndex of selected test to be used for adding results to the table
+        index = self.selectedTree.indexFromItem(item)
+        test_item['Test Name'] = item.parent().text(0)
+        test_item['Test Case'] = item.text(0)
+        test_item['Test Index'] = index
+        return test_item
+
+    # Write results
+    def _write_results(self, test_index, status):
+        pass
+
     # Retrieve checkin information
     def _get_checkin_info(self):
         self.checkin_info['device'] = self.device.currentText()
@@ -100,7 +161,7 @@ class MainWindow(QObject):
         self.checkin_info['team_member'] = self.teamMember.text()
 
         print(self.checkin_info)
-    
+
     # Enable test selection tab according to selected device
     def _device_selected(self, index: int):
         # Disable both tabs
@@ -117,7 +178,11 @@ class MainWindow(QObject):
     # Run tests
     @Slot()
     def run(self):
+        self._get_selected_tests()
+        print(self.selectedTests)
         print('run button is clicked')
+        # for i in self.selectedTests:
+        #     print(i)
 
     # Cancel tests
     @Slot()
@@ -125,7 +190,7 @@ class MainWindow(QObject):
         print('cancel button is clicked')
 
     # Run calibration test
-    @Slot() 
+    @Slot()
     def calibration(self):
         print('calibration test button is clicked')
 
@@ -136,7 +201,7 @@ class MainWindow(QObject):
         print('save button is clicked')
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainwindow = MainWindow()
     sys.exit(app.exec_())
