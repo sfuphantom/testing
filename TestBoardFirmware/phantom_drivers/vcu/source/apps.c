0cb6b295a0d19c1fa83c52587a5badbd15e671a2
courtenayhuffman <courtenay.huffman@gmail.com>
2021-03-01 19:39:56 -0800

    add new test cases

--- TestBoardFirmware/phantom_drivers/vcu/source/apps.c
+++ TestBoardFirmware/phantom_drivers/vcu/source/apps.c
@@ -7,6 +7,16 @@
 
 #include "apps.h"
 
+
+//will use spi and possibly pins 35, 33, 32 and 11 to set vals
+//apparently use halcogen to generate files?
+//find examples how to use mibspi on tms570
+
+
+//following values need to be set to proper value
+#define DEFAULT_APPS_1 1 //VALUE WITH NORMAL APPS RANGE
+#define DEFAULT_APPS_2 1 //VALUE WITH NORMAL APPS RANGE
+
 enum
 {
     NORMAL_APPS_OPERATION,
@@ -14,8 +24,9 @@ enum
     APPS_SHORT_CIRCUIT,
     APPS_OPEN_CIRCUIT,
     APPS_BSE_ACTIVATED, // APPS sensor indicates >25% activation while BSE applied
-    APPS_BSE_DEACTIVATED // after APPS & BSE applied, power returns after APPS registers <5% activation
-    APPS_OUT_OF_RANGE //values outside normal operating range, but not an open or short circuit, for more than 100ms
+    APPS_BSE_DEACTIVATED, // after APPS & BSE applied, power returns after APPS registers <5% activation
+    APPS_OUT_OF_RANGE, //values outside normal operating range, but not an open or short circuit, for more than 100ms
+    APPS_SWEEP
 };
 
 // Static function prototypes
@@ -42,6 +53,10 @@ void apps_process(uint8_t state)
             break;
         case APPS_OUT_OF_RANGE:
             apps_out_of_range();
+            break;
+        case APPS_SWEEP:
+            apps_sweep();
+            break;
         default: 
             normal_apps_operation();
             break;
@@ -94,4 +109,8 @@ static void apps_out_of_range()
     // one or both APPS sensors indicate voltage outside normal operating range (X(MIN) - X(MAX))
     // both APPS sensor values within 10% of each other
 }
+
+static void apps_sweep()
+{
+    //sweep test, check range of values
 }
--- TestBoardFirmware/phantom_drivers/vcu/source/bse.c
+++ TestBoardFirmware/phantom_drivers/vcu/source/bse.c
@@ -7,19 +7,24 @@
 
 #include "bse.h"
 
+//VALUES WILL HAVE TO BE SET PROPERLY
+#define DEFAULT_BSE 1
+
 enum
 {
     NORMAL_BSE_OPERATION,
     BSE_OPEN_CIRCUIT,
     BSE_SHORT_CIRCUIT,
-    BSE_OUT_OF_RANGE // values outside normal operating range, but not open or short for more than 100ms
+    BSE_OUT_OF_RANGE, // values outside normal operating range, but not open or short for more than 100ms
+    APPS_BSE_ACTIVATED, // APPS sensor indicates >25% activation while BSE applied
+    APPS_BSE_DEACTIVATED, // after APPS & BSE applied, power returns after APPS registers <5% activation
+    BSE_SWEEP
 };
 
 // Static function prototypes
 static void normal_bse_operation();
 
-void bse_process(uint8_t state)
-{
+void bse_process(uint8_t state){
     switch(state)
     {
         case BSE_OPEN_CIRCUIT:
@@ -31,28 +36,31 @@ void bse_process(uint8_t state)
         case BSE_OUT_OF_RANGE:
             bse_out_of_range();
             break;
+        case BSE_SWEEP:
+            bse_sweep();
+            break;
         default:
             normal_bse_operation();
             break;
     }
 }
 
-static void normal_bse_operation()
-{
+static void normal_bse_operation(){
     // bse sensor reads values between X(MIN) and X(MAX)
 }
 
-static void bse_open_circuit()
-{
+static void bse_open_circuit(){
     // bse sensor reads an open circuit (0V)
 }
 
-static void bse_short_circuit()
-{
+static void bse_short_circuit(){
     // bse sensor reads a short circuit (5V)
 }
 
-static void bse_out_of_range()
-{
+static void bse_out_of_range(){
     // bse sensor reads outside of normal operating range, but not a short or open circuit
+}
+
+static void bse_sweep(){
+
 }
