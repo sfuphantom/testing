1ec2ae1b88ec4f076d526c13f7c2fea1a1b52673
courtenayhuffman <courtenay.huffman@gmail.com>
2021-03-03 18:24:12 -0800

    fix syntax errors

--- TestBoardGUI/tests/vcu_test.py
+++ TestBoardGUI/tests/vcu_test.py
@@ -8,13 +8,14 @@
 
 #implement tests as an integration test or test fully encompassing all vcu states to create a json
 
-include pytest
+import pytest
 
 normal_vcu = '{"Mode": "VCU", "APPS": NORMAL, "BSE": NORMAL, "IMD": NORMAL, "HV_VOLTAGE": NORMAL, "HV_CURRENT": NORMAL, "TSAL": NORMAL, "LOW_VOLTAGE": NORMAL, "COMMUNICATIONS": NORMAL, "REPEAT": 1}'
+#test_cases = 
 
-def build_json()
+def build_json():
     vcu_json= '{"Mode": "VCU", "APPS": NORMAL, "BSE": NORMAL, "IMD": NORMAL, "HV_VOLTAGE": NORMAL, "HV_CURRENT": NORMAL, "TSAL": NORMAL, "LOW_VOLTAGE": NORMAL, "COMMUNICATIONS": NORMAL, "REPEAT": 1}'
     return vcu_json
 
-def test_vcu_json()
+def test_vcu_json():
     assert normal_vcu == build_json()
