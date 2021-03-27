9e78e0523943cf6c4423936fbc04d1e28874917d
courtenayhuffman <courtenay.huffman@gmail.com>
2021-03-27 00:25:58 -0700

    clean up code

--- TestBoardFirmware/phantom_drivers/common/source/MCP48FV_DAC_SPI.c
+++ TestBoardFirmware/phantom_drivers/common/source/MCP48FV_DAC_SPI.c
@@ -17,6 +17,8 @@
 #define DAC_READ_CMD 03 //0b11
 #define DAC_WRITE_CMD 00
 
+#define CMDERR 0
+
 // in some kind of definitions lib/file?
 #define DAC_SIZE_8 0xFF
 #define DAC_SIZE_10 0x3FF
@@ -27,7 +29,7 @@
 #define TRANSFERGROUP_BSE_HV 1
 #define TRANSFERGROUP_BMS 2
 
-// OLD: DAC_SPI_PORT NEW: NEED PORT DEFINITIONS FOR EACH DIFFERENT DAC, PASS INTO FUNCTIONS
+// OLD: DAC_SPI_PORT NEW: NEED PORT DEFINITIONS FOR EACH DIFFERENT DAC
 #define APPS1_SPI_PORT "DAC 2 VOUT0"
 #define APPS2_SPI_PORT "DAC 2 VOUT1"
 #define BSE_SPI_PORT "DAC 1 VOUT1"
@@ -45,23 +47,18 @@ bool MCP48FV_Init(){
 /* Main DAC controller, configure to set both output voltages from 0-5VDC
  * use: targetVoltage= 500 = 5.00V, 251 = 2.51V
 */
-bool MCP48FV_Set_Value(uint16_t targetVoltage1, uint16_t targetVoltage2, uint16_t DAC_SIZE, uint32_t transfer_group){ //pass in spi port somehow
-    if(targetVoltage1>496) // why this value? why not 500 (equivalent to 5V)?
-   {
-       targetVoltage1 = 496;
-   }
-    if(targetVoltage2>496) // why this value? why not 500 (equivalent to 5V)?
-   {
-       targetVoltage2 = 496;
-   }
+bool MCP48FV_Set_Value(uint16_t targetVoltage1, uint16_t targetVoltage2, uint16_t DAC_SIZE, uint32_t transfer_group){ 
+
+    targetVoltage1 = (targetVoltage1>496) ? 496 : targetVoltage1;
+    targetVoltage2 = (targetVoltage2>496) ? 496 : targetVoltage2;
 
     uint32_t enableBitPercent1= ((targetVoltage1)*1000)/(DAC_HIGHEST_VOLTAGE*100);
     uint32_t dacData1= (enableBitPercent1*DAC_SIZE)/1000; 
-    MCP48FV_Write(cmdCreator(DAC0_REGISTER_ADDRESS, DAC_WRITE_CMD,0,dacData1)); 
+    MCP48FV_Write(cmdCreator(DAC0_REGISTER_ADDRESS, DAC_WRITE_CMD, CMDERR, dacData1)); 
 
     uint32_t enableBitPercent2= ((targetVoltage2)*1000)/(DAC_HIGHEST_VOLTAGE*100);
     uint32_t dacData2= (enableBitPercent2*DAC_SIZE)/1000; 
-    MCP48FV_Write(cmdCreator(DAC1_REGISTER_ADDRESS, DAC_WRITE_CMD,0,dacData2)); 
+    MCP48FV_Write(cmdCreator(DAC1_REGISTER_ADDRESS, DAC_WRITE_CMD, CMDERR, dacData2)); 
 
     return true;
 }
@@ -71,14 +68,12 @@ bool MCP48FV_Set_Value(uint16_t targetVoltage1, uint16_t targetVoltage2, uint16_
 */
 bool MCP48FV_Set_Value(uint16_t targetVoltage, uint16_t DAC_SIZE, uint8_t dacVout, uint32_t transfer_group){
 
-   if(targetVoltage>496) // why this value? why not 500 (equivalent to 5V)?
-   {
-       targetVoltage = 496;
-   }
+    targetVoltage = (targetVoltage>496) ? 496 : targetVoltage;
+
     uint32_t enableBitPercent= ((targetVoltage)*1000)/(DAC_HIGHEST_VOLTAGE*100);
     uint32_t dacData= (enableBitPercent*DAC_SIZE)/1000; 
 
-   MCP48FV_Write(cmdCreator(dacVout, DAC_WRITE_CMD,0,dacData), transfer_group);
+    MCP48FV_Write(cmdCreator(dacVout, DAC_WRITE_CMD, CMDERR, dacData), transfer_group);
 
     return true;
 }
@@ -88,7 +83,6 @@ uint32_t cmdCreator(uint8_t address, uint8_t cmdReadWrite, uint8_t cmderr, uint1
     return ((address<<19) + (cmdReadWrite<<17) + (cmderr<<16) + (0<<12)+ dataBit);
 }
 
-// TO DO: ADAPT FOR ABILITY TO USE MULTIPLE DACS: (DAC_SPI_PORT WILL BE DIFFERENT HERE)
 //responsible for transmitting SPI command
 bool MCP48FV_Write(uint32_t cmdString, uint32_t transfer_group){ 
 
@@ -96,9 +90,8 @@ bool MCP48FV_Write(uint32_t cmdString, uint32_t transfer_group){
 //    uint8_t cmdSPI2= (cmdString>>8);
 //    uint8_t cmdSPI3= (cmdString>>16);
 
-//
     uint16_t txbuffer[]={(uint8_t) (cmdString>>16),(uint8_t) (cmdString>>8),(uint8_t) (cmdString>>0)};
-    mibspiSetData(DAC_SPI_PORT, transfer_group,txbuffer); // CHANGE SO THAT PORT CAN BE ADAPTED FOR MULTIPLE PORTS
+    mibspiSetData(DAC_SPI_PORT, transfer_group,txbuffer); 
     mibspiTransfer(DAC_SPI_PORT,transfer_group);
     while(!(mibspiIsTransferComplete(DAC_SPI_PORT,transfer_group))); // need a timeout
     // start a timer, don't use a while loop forever
@@ -110,7 +103,7 @@ bool MCP48FV_Write(uint32_t cmdString, uint32_t transfer_group){
 //return register data from specific register
 uint16_t readRegister (uint8_t registerAddress, uint32_t transfer_group){
 
-    MCP48FV_Write(cmdCreator(registerAddress,DAC_READ_CMD,0,0), uint32_t transfer_group);
+    MCP48FV_Write(cmdCreator(registerAddress,DAC_READ_CMD, CMDERR, 0), transfer_group);
     return  MCP48FV_Read(transfer_group) % (1<<12);
 }
 
