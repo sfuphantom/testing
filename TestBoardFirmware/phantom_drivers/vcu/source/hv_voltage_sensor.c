d18dce1ee5b21c0ed566d3b5cd2a36bab5106d87
[RyanHeo10] <43502224+RyanHeo10@users.noreply.github.com>
2021-03-30 23:11:58 -0700

    Wrote function for testing zero voltage

--- TestBoardFirmware/phantom_drivers/vcu/source/hv_voltage_sensor.c
+++ TestBoardFirmware/phantom_drivers/vcu/source/hv_voltage_sensor.c
@@ -8,10 +8,6 @@
 #include "hv_voltage_sensor.h"
 #include <math.h>
 
-# define MAX_DATA_BITS  (12u)
-# define START_INDEX    (1u)
-
-static uint8 binaryNum[12];
 static uint16_t ADC_output;
 
 enum
@@ -26,7 +22,6 @@ enum
 // Static function prototypes
 static void normal_hv_vs_operation();
 
-void hv_cs_process(uint8_t state)
 void hv_vs_process(uint8_t state)
 {
     switch(state)
@@ -49,74 +44,34 @@ void hv_vs_process(uint8_t state)
     }
 }
 
-
-static float (int battery_voltage)
 static int getADCdigital(int battery_voltage)
 {
-   float output_voltage;
    int output_voltage;
    // convert accumulator voltage to ADC output in floating form
-   if (battery_voltage >=0){
    if (battery_voltage >=144){
        output_voltage = ((battery_voltage *(4.99/479.99))-1.5)*8.2/2.048*2^12;
    }
    else if {
        output_voltage = ((1.5-(battery_voltage *(4.99/479.99)))*8.2/2.048*2^12;
    }
-   return output_voltage;
-}
-
-static void decToBinary(float ADC_input)
-{
-  int remainder, counter = 0, my_binary = 0;
-  while (integer > 0)
-{
-    remainder = integer % 2;
-    my_binary += remainder * pow(10, counter);
-    integer /= 2;
-    counter++;
-}  
-    return my_binary
-}
-
-
-static void extract12bits(int n)
-{
-    int  idx = 0;
-    // Size of an integer is assumed to be 12 bits
-    for (idx = (MAX_DATA_BITS -1) ; idx >= 0; idx--)
-    {
-        int k = n >> idx;
-        if (k & 1)
-        {
-            binaryNum[idx] = 1;
-        }
-        else
-        {
-            binaryNum[idx] = 0;
-        }
-    }
    return output_voltage
 }
 
 void getADCdigital(int input)
 {
-    float output;
-
-    adc_input = analogtodigital (input)
+    uint16_t output;
+    adc_input =  (input)
     TX_ADS7044_Slave[1] = decToBinary(adc_input);
 }
 
 static void normal_hv_vs_operation()
 {
     // HV_VS operate between 125V and 168V range
-    ADC_output_voltage = getADCdigital();
     
 }
 
 static void hv_vs_both_bounds()
 {
-    // HV_VS indicate 125V and 168V
     //sending lower bound voltage of 125
     ADC_output = getADCdigital(125)
     TX_ADS7044_Slave[1] = ADC_output;
@@ -129,7 +84,6 @@ static void hv_vs_both_bounds()
 
 static void hv_vs_out_of_range()
 {
-    // HV_VS operate outside normal operating range between 125V and 168V
     // HV_VS doesn't operate outside normal operating range between 125V and 168V
     //sending ADC output voltage below the lower bound voltage
     ADC_output = getADCdigital(120)
@@ -144,12 +98,13 @@ static void hv_vs_out_of_range()
 static void hv_vs_at_zero()
 {
     // HV_VS indicate 0 voltage
+    // sending ADC output voltage of 0
+    ADC_output =0;
     TX_ADS7044_Slave[1] = 0;
 }
 
 static void HV_VS_SWEEP()
 {
-    // Sweep test checking range of values
     //Sweep test with 1V increment from 125V to 168V
     int input_voltage = 125;
     while(input_voltage <=168)
