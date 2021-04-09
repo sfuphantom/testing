12a73c60ffc4e061db65e8a4152765bd2126d50c
Vlad Bors <vladbors@live.ca>
2021-04-08 19:59:15 -0700

    updates to lv_sensor

--- TestBoardFirmware/phantom_drivers/vcu/source/lv_sensor.c
+++ TestBoardFirmware/phantom_drivers/vcu/source/lv_sensor.c
@@ -2,7 +2,7 @@
  * lv_sensor.c
  *
  *  Created on: April 1, 2021
- *      Author: Vlad
+ *      Author: Vlad Bors
  */
 ​
 #include "lv_sensor.h"
@@ -25,21 +25,28 @@ static void lv_pow_powerdraw();
 static void lv_pow_current_short();
 static void lv_pow_current_sustained();
 ​
-void hv_cs_process(uint8_t state) // @@help
-{
+void lv_sensor_process(uint8_t state){
     switch (state)
     {
-    case LV_POW_STD_OP:
-        lv_pow_std_op();             break;
-        /*   lv_pow_uv();                 break;
-           lv_pow_ov();                 break;
-           lv_pow_powerdraw();          break;
-           lv_pow_current_short();      break;
-           lv_pow_current_sustained();  break;
-           break;
-           */
+        case LV_POW_UV:
+            lv_pow_uv();
+            break;
+        case LV_POW_OV:
+            lv_pow_ov();
+            break;
+        case LV_POW_POWERDRAW:
+            lv_pow_powerdraw();
+            break;
+        case LV_POW_CURRENT_SHORT:
+            lv_pow_current_short();
+            break;
+        case LV_POW_CURRENT_SUSTAINED:
+            lv_pow_current_sustained();
+            break;
+        default:
+            lv_pow_std_op();
+            break;
     }
-    ​
 }
 ​
 void lv_pow_std_op() {
