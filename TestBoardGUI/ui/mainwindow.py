7529f48706dfd704a6ea6fa61e2a10bb6383ce86
tnookut <nookuttanya@gmail.com>
2021-04-13 16:29:25 -0700

    Make repeat column editable and create a second page for test results

--- TestBoardGUI/ui/mainwindow.py
+++ TestBoardGUI/ui/mainwindow.py
@@ -4,12 +4,14 @@ import json
 from datetime import date
 
 from PySide2.QtWidgets import (QApplication, QPushButton, QLineEdit,
-                               QTabWidget, QTreeWidget, QComboBox, QTreeWidget,
-                               QWidget, QTreeWidgetItem, QHeaderView)
-from PySide2.QtCore import (QFile, QObject, QDir, Signal, Slot, Qt,
-                            QAbstractProxyModel)
+                               QTabWidget, QTreeWidget, QComboBox,
+                               QWidget, QStackedWidget, QCommandLinkButton,
+                               QStatusBar, QTreeWidgetItem, QAbstractItemView)
+from PySide2.QtCore import (QFile, QObject, Signal, Slot, Qt,
+                            QAbstractItemModel, QItemSelectionModel)
 from PySide2.QtUiTools import (QUiLoader)
-from PySide2.QtGui import (QBrush, QColor)
+from PySide2.QtGui import (QBrush, QColor, QStandardItemModel,
+                           QStandardItem)
 
 
 # Set-up/Connect mainwindow
@@ -20,36 +22,40 @@ class MainWindow(QObject):
 
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+        self._load_ui("mainwindow.ui")
 
         # Instantiate required objects
-        # ResultWriter displays results on the gui
+
+        # ResultsWriter displays results on the gui
         self.resultsWriter = ResultsWriter()
         # Dummy test results generator. To be replaced by real tests handler.
         self.testdummy = TestDummy()
-        # Connect signal emitted by run button to test handler.
-        self.runsignal.connect(self.testdummy.poppulate_results)
-        # Connect results signal from "dummy" test handler to ResultWriter
-        self.testdummy.results.connect(self.resultsWriter.test_function)
 
-        # Initialize Check-In Information dictionary to be filled
+        # Connect sisgnal emitted by run button to test handler.
+        self.runsignal.connect(self.testdummy.poppulate_results)
+        # Connect results signal from "dummy" test hander to ResultWriter
+        self.testdummy.results.connect(self.resultsWriter.writeResults)
+
+        # Initialize Check-In information dictionary to be filled
         self.checkin_info = {'device': '', 'board_name': '',
                              'board_version': '', 'testID': '',
                              'team_member': '', 'date': str(date.today())}
 
-        # Load ui file
-        loader = QUiLoader()
-        path = os.path.join(os.path.dirname(__file__), 'mainwindow.ui')
-        ui_file = QFile(path)
-        ui_file.open(QFile.ReadOnly)
-        self.mainwindow = loader.load(ui_file)
-        ui_file.close()
-
         # Connect Python to ui features
 
-        # ComboBox
-        self.device = self.mainwindow.findChild(QComboBox, 'deviceSelection')
-        self.device.currentTextChanged.connect(lambda: self._device_selected(
-                                               self.device.currentIndex()))
+        # QTreeWidget
+        self.VCUTree = self.mainwindow.findChild(QTreeWidget, 'VCUTreeWidget')
+        self.BMSTree = self.mainwindow.findChild(QTreeWidget, 'BMSTreeWidget')
+        self.resultsTree = self.mainwindow.findChild(
+            QTreeWidget, 'resultsTreeWidget')
+        # Pass resultsTree to resultsWriter
+        self.resultsWriter.get_results_tree(self.resultsTree)
+
+        # Combobox
+        self.deviceCombobox = self.mainwindow.findChild(
+            QComboBox, 'deviceSelection')
+        self.deviceCombobox.currentTextChanged.connect(
+            lambda: self._device_selected(self.deviceCombobox.currentIndex()))
 
         # LineEdits
         self.boardName = self.mainwindow.findChild(
@@ -61,24 +67,30 @@ class MainWindow(QObject):
             QLineEdit, 'teamMemberLineEdit')
 
         # TabWidget
-        self.tabwidget = self.mainwindow.findChild(
+        self.tabWidget = self.mainwindow.findChild(
             QTabWidget, 'deviceTabWidget')
         # Disable VCU tab
-        self.tabwidget.setTabEnabled(0, False)
+        self.tabWidget.setTabEnabled(0, False)
         # Disable BMS tab
-        self.tabwidget.setTabEnabled(1, False)
+        self.tabWidget.setTabEnabled(1, False)
         # Empty tab just to fix appearance
-        self.tabwidget.setTabVisible(2, False)
+        self.tabWidget.setTabVisible(2, False)
 
-        # TreeWidget
-        self.VCUTree = self.mainwindow.findChild(QTreeWidget, 'VCUTreeWidget')
-        self.BMSTree = self.mainwindow.findChild(QTreeWidget, 'BMSTreeWidget')
-        self.resultsTree = self.mainwindow.findChild(
-            QTreeWidget, 'resultsTreeWidget')
-        self.VCUTree.itemClicked.connect(self._duplicate_selections)
-        self.BMSTree.itemClicked.connect(self._duplicate_selections)
-        # Pass resultsTree to resultsWriter
-        self.resultsWriter.results_tree(self.resultsTree)
+        # Status Bar
+        self.status = self.mainwindow.findChild(QStatusBar, 'statusbar')
+
+        # StackedWidget
+        self.stackedWidget = self.mainwindow.findChild(
+            QStackedWidget, 'stackedWidget')
+        self.stackedWidget.setCurrentIndex(0)
+
+        # CommandLinkButtons
+        self.VCUTreeResultsButton = self.mainwindow.findChild(
+            QCommandLinkButton, 'viewResults')
+        self.VCUTreeResultsButton.clicked.connect(self.view_results)
+        self.backButton = self.mainwindow.findChild(
+            QCommandLinkButton, 'back')
+        self.backButton.clicked.connect(self.view_test_cases)
 
         # PushButtons
         self.calibrationTestButton = self.mainwindow.findChild(
@@ -97,80 +109,93 @@ class MainWindow(QObject):
         # Show ui when application runs
         self.mainwindow.show()
 
-    # Populate test cases in test selection tree
-    def _fill_test_cases(self, device: str):
-        # Select tree to populate base on selected device
-        if device == 'VCU':
+    # Load ui file
+    def _load_ui(self, file):
+        loader = QUiLoader()
+        path = os.path.join(os.path.dirname(__file__), file)
+        ui_file = QFile(path)
+        ui_file.open(QFile.ReadOnly)
+        self.mainwindow = loader.load(ui_file)
+        ui_file.close()
+
+    # Enable test selection tab according to selected device
+    def _device_selected(self, index: int):
+        # Disable both tabs
+        self.tabWidget.setTabEnabled(0, False)
+        self.tabWidget.setTabEnabled(1, False)
+
+        # Switch to selected device tab
+        self.tabWidget.setCurrentIndex(index)
+        self.tabWidget.setTabEnabled(index, True)
+
+        # Get selected device
+        self.device = self.deviceCombobox.currentText()
+
+        # Define selected tree based on selected device
+        if self.device == 'VCU':
             self.selectedTree = self.VCUTree
-        elif device == 'BMS':
+        elif self.device == 'BMS':
             self.selectedTree = self.BMSTree
 
-        # Load testcases JSON
-        path = os.path.join(os.path.dirname(__file__), 'testcases.json')
-        with open(path) as f:
-            test_cases = json.load(f)
+        # Set tree behaviors
+        self.selectedTree.setSelectionMode(QAbstractItemView.MultiSelection)
+        # WIP: SelectedClicked doesn't work for some reason. IDK
+        self.selectedTree.setEditTriggers(QAbstractItemView.SelectedClicked
+                                          | QAbstractItemView.DoubleClicked)
 
-        # Populate QTreeWidget
-        for thing in test_cases[device]:
-            for key, value in thing.items():
+        # Get test attributes from testcases.JSON
+        self.test_attributes = self._get_json_attributes(self.device)
+
+        # Populate test selection
+        self._fill_test_cases(self.test_attributes)
+
+    # Populate test cases in test selection tree
+    def _fill_test_cases(self, test_attributes):
+        for group in test_attributes:
+            for test_name, test_cases in group.items():
                 # Populate parents (test names)
-                parent = QTreeWidgetItem([key])
+                parent = QTreeWidgetItem([test_name])
                 self.selectedTree.addTopLevelItems([parent])
-                # Add check box to parent
-                # parent.setCheckState(0, Qt.Unchecked)
                 # Disable selection of parents
                 parent.setFlags(parent.flags() & ~Qt.ItemIsSelectable)
                 # Populate children (test cases)
-                for val in value:
-                    children = QTreeWidgetItem(list(val.values()))
-                    parent.addChildren([children])
-                    # Add check box to each test case
-                    # children.setCheckState(0, Qt.Unchecked)
+                for case in test_cases:
+                    column_0 = case['Case']
+                    column_1 = case['Repeat']
+                    column_2 = case['Description']
+                    child = QTreeWidgetItem([column_0, column_1, column_2])
+                    parent.addChild(child)
+                    # Make children editable
+                    child.setFlags(child.flags() | Qt.ItemIsEditable
+                                   | Qt.ItemIsEnabled | Qt.ItemIsSelectable)
 
-    # WIP: This function is buggy. 
-    @Slot()
-    def _duplicate_selections(self):
-        items = self.selectedTree.selectedItems()
-        self.resultsTree.clear()
-        for item in items:
-            # why did i create this index smh not even using it
-            # index = items.index(item)
-            parent = QTreeWidgetItem([item.parent().text(0)])
-            self.resultsTree.addTopLevelItem(parent)
-            child = QTreeWidgetItem([item.text(0)])
-            parent.addChild(child)
+    # Get test attributes from testcases.JSON for selected device
+    def _get_json_attributes(self, device: str):
+        # Load testcases.json
+        path = os.path.join(os.path.dirname(__file__), 'testcases.json')
+        with open(path) as f:
+            test_cases = json.load(f)
+        return test_cases[device]
 
     # Retrieve selected test cases
     def _get_selected_tests(self):
-        # Retrieve all selected tests in -> list(QTreeWidgetItems)
-        self._rawSelection = self.selectedTree.selectedItems()
-
-        test_item = {'Test Name': '', 'Test Case': '', 'Repeat': None}
-
-        self.selectedTests = list()
-
-        for item in self._rawSelection:
-            test_item = self._parse_tests(item)
-            self.selectedTests.append(test_item)
+        rawSelections = self.selectedTree.selectedItems()
+        selectedTests = list()
+        for selection in rawSelections:
+            selectedTests.append(self._parse_tests(selection))
+        return selectedTests
 
     # Parse user test selection
     def _parse_tests(self, item):
-        test_item = {}
+        test_item = dict()
         test_item['Test Name'] = item.parent().text(0)
         test_item['Test Case'] = item.text(0)
-        # WIP: repeat column doesn't exist in test selection yet
-        # test_item['Repeat'] = item.text(1)
-        test_item['Repeat'] = None
-
-        # QModelIndex of selected test to be used for adding results to the table
-        # index = self.selectedTree.indexFromItem(item)
-        # This index is from test selection tree. Cannot be used with results tree.
-        # test_item['Test Index'] = index
+        test_item['Repeat'] = item.text(1)
         return test_item
 
-    # Retrieve checkin information
+    # Retrieve check-in information
     def _get_checkin_info(self):
-        self.checkin_info['device'] = self.device.currentText()
+        self.checkin_info['device'] = self.deviceCombobox.currentText()
         self.checkin_info['board_name'] = self.boardName.text()
         self.checkin_info['board_version'] = self.boardVersion.text()
         self.checkin_info['testID'] = self.testID.text()
@@ -178,39 +203,57 @@ class MainWindow(QObject):
 
         print(self.checkin_info)
 
-    # Enable test selection tab according to selected device
-    def _device_selected(self, index: int):
-        # Disable both tabs
-        self.tabwidget.setTabEnabled(0, False)
-        self.tabwidget.setTabEnabled(1, False)
-
-        # Switch to selected device tab
-        self.tabwidget.setCurrentIndex(index)
-        self.tabwidget.setTabEnabled(index, True)
-
-        # Populate test selection
-        self._fill_test_cases(self.device.currentText())
+    # Disable/Enable some features
+    def _switch_mode(self, mode):
+        # Diable run button
+        self.runPushButton.setEnabled(mode)
+        # Disable combobox
+        self.deviceCombobox.setEnabled(mode)
+        # Disable calibration test button
+        self.calibrationTestButton.setEnabled(mode)
+        # Disable all line edits\
+        self.boardName.setEnabled(mode)
+        self.testID.setEnabled(mode)
+        self.boardVersion.setEnabled(mode)
+        self.teamMember.setEnabled(mode)
 
     # Run tests
     @Slot()
     def run(self):
-        self._get_selected_tests()
+        # Clean results tree before showing showing results
+        self.resultsTree.clear()
+        self.status.showMessage('Tests in progress...')
+        # Get selected tests
+        self.selectedTests = self._get_selected_tests()
         print(self.selectedTests)
-        print('run button is clicked')
-        # for i in self.selectedTests:
-        #     print(i)
+        # Disable some features while running tests
+        self._switch_mode(False)
+        '''
+        Excecute run code here
+
+        '''
         # Emit selectedTests to runsignal
         self.runsignal.emit(self.selectedTests)
+        # Switch to result page
+        self.stackedWidget.setCurrentIndex(1)
 
     # Cancel tests
     @Slot()
     def cancel(self):
         print('cancel button is clicked')
+        '''
+        Write something here to disrupt tests if needed
+
+        '''
 
     # Run calibration test
     @Slot()
     def calibration(self):
         print('calibration test button is clicked')
+        '''
+        Excecute calibration test code here
+
+        '''
 
     # Save test reults
     @Slot()
@@ -218,9 +261,18 @@ class MainWindow(QObject):
         self._get_checkin_info()
         print('save button is clicked')
 
-    # Dont mind me. Just being tested.
-    def test_function(self):
-        print('Hi there! I am test_function. I belong in MainWindow. :)')
+    @Slot()
+    def view_results(self):
+        self.stackedWidget.setCurrentIndex(1)
+        self.status.showMessage('Viewing test results')
+        print('view results')
+
+    @Slot()
+    def view_test_cases(self):
+        self.stackedWidget.setCurrentIndex(0)
+        self.status.showMessage('Selecting test cases.')
+        print('view test cases')
+        self._switch_mode(True)
 
 
 # This class display results on the gui.
@@ -229,39 +281,33 @@ class ResultsWriter(QObject):
         super(ResultsWriter, self).__init__()
 
     # Retreive results tree from MainWindow in order to edit.
-    def results_tree(self, tree):
-        self.results_tree = tree
+    def get_results_tree(self, tree):
+        self.resultsTree = tree
 
     # Display status as PASS/FAIL in green/red
-    def _fill_status(self, index, status: bool):
-        status = "PASS" if status else "FAIL"
-        colour = "#49E20E" if status else "#FF0000"
-        # Edit status column in results tree
-        self.results_tree.itemFromIndex(index).setText(2, status)
-        # Change colour of text
-        self.results_tree.itemFromIndex(index).setForeground(
-            2, QBrush(QColor(colour)))
-
-    # Display message in results tree
-    def _fill_message(self, tree, index, message: str):
-        self.results_tree.itemFromIndex(index).setText(3, message)
+    def _fill_status(self, stat: bool, child):
+        status = "PASS" if stat else "FAIL"
+        colour = "#196F3D" if stat else "#FF0000"
+        child.setText(1, status)
+        child.setForeground(1, QBrush(QColor(colour)))
 
     # Write status and message for each test item
-    @Slot(list)
-    def writeresults(self, results: list):
-        for test in results:
-            index = test['Test Index']
-            status = test['Test Status']
-            message = test['Test Message']
-            self._fill_status(index, status)
-            self._fill_message(index, message)
+    @ Slot(list)
+    def writeResults(self, results: list):
+        self._fill_tree(results)
 
-    # Dont mind me. Just being tested.
-    @Slot(list)
-    def test_function(self, results):
-        print('hi there. I belong in results writer.')
-        for i in results:
-            print(i)
+    def _fill_tree(self, data):
+        for item in data:
+            parent = QTreeWidgetItem([item['Test Name']])
+            parent.setFlags(parent.flags() & ~Qt.ItemIsSelectable)
+            self.resultsTree.addTopLevelItems([parent])
+            self.resultsTree.expandItem(parent)
+            column_0 = item['Test Case']
+            column_2 = item['Message']
+            child = QTreeWidgetItem([column_0, '', column_2])
+            parent.addChild(child)
+            self._fill_status(item['Status'], child)
+            child.setFlags(child.flags() & ~Qt.ItemIsSelectable)
 
 
 # Dont mind me. Just a dummy test handler.
@@ -282,7 +328,6 @@ class TestDummy(QObject):
         for test in tests:
             test['Status'] = dummy_status
             test['Message'] = dummy_message + str(num)
-            test['Repeat'] = num
             num += 1
             dummy_status = not dummy_status
         self.results.emit(tests)
@@ -291,5 +336,4 @@ class TestDummy(QObject):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainwindow = MainWindow()
-    results = ResultsWriter()
     sys.exit(app.exec_())
