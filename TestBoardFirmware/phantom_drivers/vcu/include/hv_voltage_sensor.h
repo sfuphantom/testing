2b668ef27002f0545120bd2a669c6e76ac673d0a
[RyanHeo10] <43502224+RyanHeo10@users.noreply.github.com>
2021-05-12 18:11:24 -0700

    Updated HV VT testing files

--- TestBoardFirmware/phantom_drivers/vcu/include/hv_voltage_sensor.h
+++ TestBoardFirmware/phantom_drivers/vcu/include/hv_voltage_sensor.h
@@ -3,14 +3,43 @@
  *
  *  Created on: Nov 10, 2020 
  *      Author: Sumreen Rattan
+ *      Modified by: Ryan Heo
  */
 
 #ifndef PHANTOM_DRIVERS_INCLUDE_HV_VOLTAGE_SENSOR_H_
 #define PHANTOM_DRIVERS_INCLUDE_HV_VOLTAGE_SENSOR_H_
 
+#include "sys_common.h"
 #include "stdint.h"
+#include "mibspi.h"
+#include "gio.h"
+#include "sys_vim.h"
+#include "sys_core.h"
+//#include "timer.c"
 
+typedef enum
+{
+    HV_VS_LOWER_BOUND,
+    HV_VS_UPPER_BOUND,
+    HV_VS_OUT_OF_LOWERBOUND,
+    HV_VS_OUT_OF_UPPERBOUND,
+    HV_VS_AT_ZERO,
+    HV_VS_SWEEP
+} testcases_name;
+
+// Static function prototypes
+static void hv_vs_both_bounds();
+static void hv_vs_out_of_lowerBound();
+static void hv_vs_out_of_upperBound();
+static void hv_vs_lower_bound();
+static void hv_vs_upper_bound();
+static void hv_vs_at_zero();
+static void hv_vs_sweep();
+static int getADCdigital(int battery_voltage);
+static void spiSetup(uint16 voltage);
 void hv_vs_process(uint8_t state);
-
+static int twosComplement(int negative_output);
+void mibspiGroupNotification(mibspiBASE_t *mibspi, uint32 group);
+void adcSlaveDataSetup();
 
 #endif /* PHANTOM_DRIVERS_INCLUDE_HV_VOLTAGE_SENSOR_H_ */
--- TestBoardFirmware/phantom_drivers/vcu/source/hv_voltage_sensor.c
+++ TestBoardFirmware/phantom_drivers/vcu/source/hv_voltage_sensor.c
@@ -1,44 +1,55 @@
 /*
  * hv_voltage_sensor.c
  *
- *  Created on: Mar 16, 2021 
+ *  Created on: Mar 16, 2021
  *      Author: Ryan Heo
  */
 
+#include "sys_common.h"
 #include "hv_voltage_sensor.h"
+#include "mibspi.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
-static uint16_t ADC_output;
+#define TransferGroup0 0
+#define TransferGroup1 1
 
-enum
-{
-    NORMAL_HV_VS_OPERATION,
-    HV_VS_BOTH_BOUNDS,
-    HV_VS_OUT_OF_RANGE,
-    HV_VS_AT_ZERO,
-    HV_VS_SWEEP
-};
+uint16 ADC_output;
 
-// Static function prototypes
-static void normal_hv_vs_operation();
-static void hv_vs_both_bounds();
-static void hv_vs_out_of_range();
-static void hv_vs_both_bounds();
-static void hv_vs_at_zero();
-static void hv_vs_sweep();
+/* Transfer Group 0 */
+/* Initial data to be sent the very first time on power up to the ADC
+ * NOTE: May or may not need to be changed
+ *
+ */
+uint16 TX_Data_Master[1] = {0xAAAA};
+uint16 TX_Data_Slave[1]  = {0};
+uint16 RX_Data_Master[1] = {0};
+uint16 RX_Data_Slave[1]  = {0};
+
+uint16 RX_Yash_Master[1]   = {0};
+uint16 RX_ADS7044_Slave[1] = {0};
+
+bool TX_AVAILABLE = false;  // flags to only transfer mibspi data from slave when current transfer has finished
+bool tx_master = false;     // flags to only transfer mibspi data from master when current transfer has finished
+
+#define hv_vs_timer_PERIOD 500;
 
 void hv_vs_process(uint8_t state)
 {
     switch(state)
     {
-        case NORMAL_HV_VS_OPERATION:
-            normal_hv_vs_operation();
+        case HV_VS_LOWER_BOUND:
+            hv_vs_lower_bound();
             break;
-        case HV_VS_BOTH_BOUNDS:
-            hv_vs_both_bounds();
+        case HV_VS_UPPER_BOUND:
+            hv_vs_upper_bound();
             break;
-        case HV_VS_OUT_OF_RANGE:
-            hv_vs_out_of_range();
+        case HV_VS_OUT_OF_LOWERBOUND:
+            hv_vs_out_of_lowerBound();
+            break;
+        case HV_VS_OUT_OF_UPPERBOUND:
+            hv_vs_out_of_upperBound();
             break;
         case HV_VS_AT_ZERO:
             hv_vs_at_zero();
@@ -49,81 +60,180 @@ void hv_vs_process(uint8_t state)
     }
 }
 
+static int twosComplement(int negative_output){
+    negative_output=negative_output*(-1);
+    int carry = 1;
+    int binary[]={1,0,0,0,0,0,0,0,0,0,0,0};
+    int twoscomplement=0;
+    int i;
+
+    // converting to a binary value in ones complement form
+    for (i=11;negative_output>0;i--){
+        binary[i]=negative_output%2;
+        negative_output=negative_output/2;
+    }
+
+    // converting to ones'complement
+    for (i=1;i<=11;i++){
+        if (binary[i]==0){
+            binary[i]=1;
+        }
+        else if(binary[i]==1){
+            binary[i]=0;
+        }
+    }
+
+    // converting to twos'complement
+    for (i=11;i>0;i--){
+        if(binary[i] == 1 && carry == 1){
+            binary[i] = 0;
+        }
+        else if(binary[i] == 0 && carry == 1){
+            binary[i] = 1;
+            carry = 0;
+        }
+        else{
+            binary[i] = binary[i];
+        }
+    }
+
+    // converting back to an unsigned decimal
+    for (i=11;i>=0;i--){
+        int exp =0;
+        twoscomplement = twoscomplement+binary[i]*pow(2,exp);
+        exp +=1;
+    }
+    return twoscomplement;
+}
+
 static int getADCdigital(int battery_voltage)
 {
    int output_voltage;
-   // convert accumulator voltage to ADC output in floating form
    // convert accumulator voltage to ADC output in integer form
    if (battery_voltage >=144){
-       output_voltage = ((battery_voltage *(4.99/479.99))-1.5)*8.2/2.048*2^12;
+       output_voltage = (int)(((battery_voltage *(4.99/479.99))-1.5)*8.2/2.0475*2047);
    }
-   else if {
-       output_voltage = ((1.5-(battery_voltage *(4.99/479.99)))*8.2/2.048*2^12;
+   else{
+       output_voltage = (int)((1.5-(battery_voltage *(4.99/479.99)))*8.2/(-2.0475)*2047);
+       if(output_voltage<0){
+           output_voltage = twosComplement(output_voltage);
+       }
    }
-   return output_voltage
+   return output_voltage;
 }
 
-void getADCdigital(int input)
-{
-    uint16_t output;
-    adc_input =  (input)
-    TX_ADS7044_Slave[1] = decToBinary(adc_input);
-}
-
-static void normal_hv_vs_operation()
-{
-    // HV_VS operate between 125V and 168V range
-    
-    // HV_VS operate between 123V and 168V range
-        int current_voltage = 123;
-        while(input_voltage <=168)
-            ADC_output = getADCdigital(120)
-            TX_ADS7044_Slave[1] = 0;
-            //time delay?
-            current_voltage += 5;
-}
-
-static void hv_vs_both_bounds()
+static void hv_vs_lower_bound()
 {
     //sending lower bound voltage of 125
-    ADC_output = getADCdigital(125)
-    TX_ADS7044_Slave[1] = ADC_output;
-        
-    //wait for some time after sending first data?
-    //sending upper bound voltage of 168
-    ADC_output = getADCdigital(168)
-    TX_ADS7044_Slave[1] = ADC_output;
+    ADC_output = (uint16)getADCdigital(125);
+    spiSetup(ADC_output);
 }
 
-static void hv_vs_out_of_range()
+static void hv_vs_upper_bound(){
+    //sending upper bound voltage of 168
+    ADC_output = (uint16)getADCdigital(168);
+    spiSetup(ADC_output);
+}
+
+static void hv_vs_out_of_lowerBound()
 {
     // HV_VS doesn't operate outside normal operating range between 125V and 168V
-    //sending ADC output voltage below the lower bound voltage
-    ADC_output = getADCdigital(120)
-    TX_ADS7044_Slave[1] = ADC_output;
-           
-    //wait for some time after sending first data?
-    //sending ADC output voltage above the upper bound voltage
-    ADC_output = getADCdigital(170)
-    TX_ADS7044_Slave[1] = ADC_output;
+    //sending ADC output voltage below the lower bound voltage of 125V
+    ADC_output = (uint16)getADCdigital(120);
+    spiSetup(ADC_output);
+}
+
+static void hv_vs_out_of_upperBound()
+{
+    //sending ADC output voltage above the upper bound voltage of 168V
+    ADC_output = (uint16)getADCdigital(170);
+    spiSetup(ADC_output);
 }
 
 static void hv_vs_at_zero()
 {
     // HV_VS indicate 0 voltage
     // sending ADC output voltage of 0
-    ADC_output =0;
-    TX_ADS7044_Slave[1] = 0;
+    ADC_output = (uint16)0;
+    spiSetup(ADC_output);
 }
 
-static void HV_VS_SWEEP()
+/* Sweep test with a timer */
+
+/*void hv_vs_timer(Timer timer, int ID){
+
+    #ifdef TIMER_DEBUG
+    UARTprintf("hv_vs sweep timer expired.\n\n\r");
+    #endif
+
+    //increment cycle
+
+    setTimerID( BSE, ++ID );
+
+}*/
+
 static void hv_vs_sweep()
 {
     //Sweep test with 1V increment from 125V to 168V
     int input_voltage = 125;
-    while(input_voltage <=168)
-        ADC_output = getADCdigital(120)
-        TX_ADS7044_Slave[1] = 0;
-        //time delay?
+    while(input_voltage <=168){
+        ADC_output = (uint16)getADCdigital(input_voltage);
+        spiSetup(ADC_output);
+        //startTimer(APPS, SWEEP_TIMER, hv_vs_timer_PERIOD);
         input_voltage += 1;
+    }
+}
+
+static void spiSetup(uint16 voltage)
+{
+    mibspiSetData(mibspiREG3, TransferGroup1, &voltage);
+    mibspiEnableGroupNotification(mibspiREG3, TransferGroup1, 0);
+    mibspiTransfer(mibspiREG3,TransferGroup1);
+}
+
+void mibspiGroupNotification(mibspiBASE_t *mibspi, uint32 group)
+{
+
+    if (mibspi == mibspiREG1 && group == TransferGroup0)
+     {
+         mibspiDisableGroupNotification(mibspiREG1, TransferGroup0);
+         mibspiGetData(mibspi, group, RX_Data_Master);
+         tx_master = true;
+     }
+
+    if (mibspi == mibspiREG1 && group == TransferGroup1)
+    {
+        mibspiDisableGroupNotification(mibspiREG1, TransferGroup1);
+        mibspiGetData(mibspi, group, RX_Yash_Master);
+        tx_master = true;
+    }
+
+    /**********************************
+     *  TESTING FOR SLAVE FUNCTIONALITY
+     ***********************************/
+
+        if (mibspi == mibspiREG3 && group == TransferGroup1)
+        {
+            mibspiDisableGroupNotification(mibspiREG3, TransferGroup1);
+            mibspiGetData(mibspi, group, RX_ADS7044_Slave);
+            TX_AVAILABLE = true;
+        }
+
+        if (mibspi == mibspiREG3 && group == TransferGroup0)
+        {
+            mibspiDisableGroupNotification(mibspiREG3, TransferGroup0);
+            TX_AVAILABLE = true;
+        }
+}
+
+/*****************************************************************************
+ *                 ADC SLAVE SETUP FUNCTIONS - DO NOT MODIFY
+ *****************************************************************************/
+/* used for ramping up and down the measured voltage simulated by the ADC */
+
+void adcSlaveDataSetup()
+{
+    mibspiSetData(mibspiREG3, TransferGroup0, TX_Data_Slave);
+    mibspiEnableGroupNotification(mibspiREG3, TransferGroup0, 0);
+    mibspiTransfer(mibspiREG3, TransferGroup0);
 }
