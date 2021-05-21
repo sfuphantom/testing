22a0fb0e336418558efdb5c0f19472659791381c
cushingnathan37@gmail.com <73880893+nathan-37@users.noreply.github.com>
2021-05-20 20:23:35 -0700

    hvct pseudocode complete

--- TestBoardFirmware/phantom_drivers/vcu/source/hv_current_sensor.c
+++ TestBoardFirmware/phantom_drivers/vcu/source/hv_current_sensor.c
@@ -1,50 +1,110 @@
 /*
  * hv_current_sensor.c
  *
- *  Created on: Nov 10, 2020 
- *      Author: Sumreen Rattan
+ *      Author: Nathan Cushing
  */
 
 #include "hv_current_sensor.h"
 
-typedef enum operation
-{
-    NORMAL_HV_CS_OPERATION,
-    ZERO_CURRENT, // 0 Amps 
-    MAX_CURRENT, //5V or 300Amps 
-    SWEEP,
-    CURRENT_WITHOUT_APPS
-}operation;
+enum operation 
+{  
+    INTERVAL_HV_CT_RANGE //send range of voltages at .125V intervals
+    GRANULAR_HV_CT_RANGE, //send range of voltages at .001V intervals
+    OFFLINE_HV_CT, //send 0V
+    PROPORTIONAL_APPS, //send voltages at .125 intervals, alongside APPS readings
+};
 
-// Static function prototypes
-static void normal_hv_cs_operation();
-static float send_current(int CURRENT_VALUE);
+//Test function prototypes
+static void interval_hv_ct_range();
+static void granular_hv_ct_range();
+static void offline_hv_ct();
+static void proportional_apps();
+static void low_apps();
+static void high_apps();
 
-void hv_cs_process(uint8_t state)
+//Other function prototypes
+static float send_hvct_current(int a);
+static float send_apps_current(int a);
+
+//Variable notes
+    //The voltages can be stored as ints, where the range is from 0 to 3750, corrosponding to .625 - 4.375V.
+
+//Test 1
+void interval_hv_ct_range()
 {
-    switch(state)
+    int currentInt = 0;
+    for(int i = 0; i<30;i++)
     {
-        case NORMAL_HV_CS_OPERATION:
-            normal_hv_cs_operation();
-            break;
+        currentInt = i*125;
+        send_hvct_current(currentInt);
     }
 }
 
-
-/*
-* Fn: normal_hv_cs_operation
-* Purpose:   
-*/
-static void normal_hv_cs_operation()
+//Test 2
+void granular_hv_ct_range()
 {
-
+    int currentInt = 0;
+    for(int i = 0; i<3750;i++)
+    {
+        currentInt = i*125;
+        send_hvct_current(currentInt);
+    }
+    //for this one, make sure to make .csv files to graph the the values after, and they should be roughly proportional
+    //max uncertainty can be 4amps 
 }
 
-/*
-* Fn: send_current
-* Purpose: helper function, user defines the current and this function converts to an voltage output  
-*/
-static float send_current(int CURRENT_VALUE)
+//Test 3
+void offline_hv_ct()
 {
-    return 0;
+    send_hvct_current(0);
+}
+
+//Test 4
+void proportional_apps()
+{
+    int currentInt = 0;
+    for(int i = 0; i<30;i++)
+    {
+        currentInt = i*125;
+        send_hvct_current(currentInt);
+        send_apps_current(currentInt);
+    }
+}
+
+//Test 5
+void low_apps()
+{
+    int currentInt = 0;
+    for(int i = 0; i<30;i++)
+    {
+        currentInt = i*125;
+        send_hvct_current(currentInt);
+        send_apps_current(currentInt-125);
+    }
+}
+
+//Test 6
+void high_apps()
+{
+    int currentInt = 0;
+    for(int i = 0; i<30;i++)
+    {
+        currentInt = i*125;
+        send_hvct_current(currentInt);
+        send_apps_current(currentInt+125);
+    }
+}
+
+//Other functions
+void send_hvct_current(int currentInt)
+{
+    currentInt = currentInt*.001 + .625
+    //convert int to float
+    //send the current to the VCU
+}
+
+void send_apps_current(int currentInt)
+{
+    //convert int to what the APPS should be. ie, with 200A, there should be x amount of the pedal pushed down.
+    //send the apps value to the VCU
 }
