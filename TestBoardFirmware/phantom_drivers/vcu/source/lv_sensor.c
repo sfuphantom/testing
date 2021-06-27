ae95ad22f3063b79f839f4dd6b0c645699ad8263
Vlad Bors <vladbors@live.ca>
2021-06-27 09:29:11 -0700

    lv_monitor updates

--- TestBoardFirmware/phantom_drivers/vcu/source/lv_sensor.c
+++ TestBoardFirmware/phantom_drivers/vcu/source/lv_sensor.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * lv_sensor.c
  *
@@ -63,9 +62,7 @@ void lv_sensor_process(uint8_t state){
 
 
 
-
-
-​/*
+/*
 static int twosComplement(int negative_output){
     negative_output=negative_output*(-1);
     int carry = 1;
@@ -147,6 +144,7 @@ int main()
 }
 */
 
+
 //uint8 LV_Calibration[2] = {0x14,0x00}; //this is a calculated value 5120->0x1400
 
 
@@ -215,43 +213,3 @@ void lv_pow_overcurrent() {
     // overcurrent protection, short a sustained signal to shunt
      current_output = (uint16)getCurrent(13.2);​
 }
-=======
-///*
-// * lv_power_sensor_test.c
-// *
-// *  Created on: Jan 23, 2021
-// *      Author: andrei
-// */
-//
-//#include "lv_sensor.h"
-//typedef enum operation
-//{
-//    NORMAL_LV_POWER_SENSOR_OPERATION
-//}; operation
-//
-//void lv_pow_std_op();
-//void lv_pow_uv();
-//void lv_pow_ov();
-//void lv_pow_powerdraw();
-//void lv_pow_current_short();
-//void lv_pow_current_sustained();
-//void hv_cs_process(uint8_t state)
-//{
-//    switch(state)
-//    {
-//        case NORMAL_LV_POWER_SENSOR_OPERATION:
-//            lv_pow_std_op();        break;
-//            lv_pow_uv();            break;
-//            lv_pow_ov();            break;
-//            lv_pow_powerdraw();     break;
-//            lv_pow_current_short(); break;
-//            lv_pow_std_op();        break;
-//            lv_pow_current_sust();  break;
-//            default;    break;
-//    }
-//}
-//
-//void lv_pow_std_op(){
-//    uint16_t mode;
-//}
->>>>>>> master
