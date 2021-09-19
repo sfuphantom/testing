9bb2694564260b0a7fe58f73b0f333d2394cb91c
rafguevara14 <66582348+rafguevara14@users.noreply.github.com>
2021-09-19 14:17:30 -0700

    add function implementations and static variables

--- TestBoardFirmware/phantom_drivers/common/source/shutdown.c
+++ TestBoardFirmware/phantom_drivers/common/source/shutdown.c
@@ -9,13 +9,31 @@
 
 /* Shutdown Variables */
 
-static shutdown_fail;
+static uint8_t shutdown_timeout; // tells user if timeout has occurred
 
-static shutdown_signal;
+static uint8_t shutdown_pass; // indicates pass or fail w.r.t. the shutdown signal
 
-static shutdown_expected;
+static uint8_t shutdown_signal; // stores shutdown GPIO value
 
-/* Shutdown Interrupts */
+static uint8_t shutdown_expected; // indicates the expected result from shutdown; set by the user but defaulted to false
+
+/* Static Functions */
+
+static void readShutdownSignal(){
+
+    shutdown_signal = gioGetBit( VCU_FLT_PORT, VCU_FLT_PIN );
+}
+
+static void resetShutdownVars(){
+
+    readShutdownSignal();
+
+    shutdown_pass     = false;
+
+    shutdown_timeout  = false;
+
+    shutdown_expected = false;
+}
 
 void initializeShutdownInterrupt(){
 
@@ -23,66 +41,98 @@ void initializeShutdownInterrupt(){
 
     vimInit();
 
+    gioInit();
+
     //Enable Interrupts
     edgeEnableNotification(VCU_FLT_REG, VCU_FLT_EDGE);
 
     _enable_IRQ();
 
-}
-
-static void readShutdownSignal(){
-
-    shutdown_signal = gioGetBit( VCU_FLT_PORT, VCU_FLT_PIN );
+    resetShutdownVars();
 }
 
 
-void shutdown_timeout_callback(int ID){
+/* Getters and Setters */
 
-    shutdown_fail = true;
+// timeout     pass     result
+//  0           0        false (either...interrupt was triggered and for some reason the result was unexpected -> test run is stopped OR nothing has happened yet so the test keeps waiting)
+//  0           1        true  (interrupt was triggered as expected and test can move on)
+//  1           0        test run is already stopped (validation stage will commence and check for timeout or pass)
+//  1           1        test run is already stopped (validation stage will commence and check for timeout or pass)
 
-    UARTprintf("SHUTDOWN SIGNAL HAS TIMED OUT\r\n");
+uint8_t isShutdownPass(){
 
-    stopAllTimers();
-
+    return shutdown_pass;
 }
 
-void shutdown_callback(){
+uint8_t isShutdownTimeout(){
 
-    if( shutdown_expected == getShutdownSignal() ){
-
-        initializeVCU();
-
-
-    }else{
-
-        stopAllTimers();
-
-        shutdown_fail = true;
-
-    }
-
+    return shutdown_timeout;
 }
-
 
 uint8_t getShutdownSignal(){
 
     return shutdown_signal;
 }
 
+uint8_t getExpectedShutdownResult(){
+
+    return expected_shutdown;
+}
+
+/*
+* Fn: setShutdownOccurence
+* Purpose: Sets the expected result of shutdown and immediately starts a timeout
+*/
 void setShutdownOccurence(bool expected_result){
 
-    setTimerID(VALIDATION, 0)
 
+    resetShutdownVars();
+
+    // Start shutdown timeout
+    setTimerID(VALIDATION, 0);
     setTimerCallback(VALIDATION, shutdown_timeout_callback);
-
     startTimer(VALIDATION, SHUTDOWN_TIMEOUT_PERIOD);
 
+    // set expected shutdown
     shutdown_expected = expected_result;
 }
 
-bool getShutdownResult(){
+/* Callback Functions */
 
-    return shutdown_fail;
+void shutdown_timeout_callback(int ID){
+
+    shutdown_pass = false; // test has FAILED
+
+    shutdown_timeout = true;
+
+    UARTprintf("SHUTDOWN SIGNAL HAS TIMED OUT\r\n");
+
+    readShutdownSignal();
+
+    stopAllTimers(); // stop test run
+}
+
+void shutdown_callback(){
+
+    readShutdownSignal();
+
+    if( shutdown_expected == getShutdownSignal() ){
+
+        //TODO: MAKE FUNCTION CALL TO INITIALIZE/RESET BMS AS WELL DEPENDING ON TEST BOARD MODE
+
+        initializeVCU(); // this signal was expected; reset the VCU and continue with test run
+
+        shutdown_pass = true; // test has PASSED
+
+    }else{
+
+        stopAllTimers(); // signal was unexpected; stop test run and fail test w.r.t. the shutdown signal
+
+        shutdown_pass = false;
+    }
+
+    setShutdownOcurrence(false); // reset expected result to false state
 }
 
 void edgeNotification(hetBASE_t * hetREG,uint32 edge)
@@ -92,12 +142,8 @@ void edgeNotification(hetBASE_t * hetREG,uint32 edge)
 
     if(hetREG == VCU_FLT_REG && edge == VCU_FLT_EDGE){
 
-        readShutdownSignal();
-
         shutdown_callback();
     }
-
-
 
 /* USER CODE END */
 }
