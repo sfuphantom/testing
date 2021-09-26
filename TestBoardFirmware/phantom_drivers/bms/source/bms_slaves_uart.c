4422399cd87591fb91f23d2b2373be0ba86cb220
courtenayhuffman <courtenay.huffman@gmail.com>
2021-09-26 16:37:03 -0700

    add data structure initialization

--- TestBoardFirmware/phantom_drivers/bms/source/bms_slaves_uart.c
+++ TestBoardFirmware/phantom_drivers/bms/source/bms_slaves_uart.c
@@ -10,6 +10,7 @@
 
 #include "bms_slaves_uart.h"
 
+static void slaves_uart_init();
 static void normal_slaves_uart();
 static void slaves_uart_undervoltage();
 static void slaves_uart_overvoltage();
@@ -17,6 +18,38 @@ static void slaves_uart_undertemperature();
 static void slaves_uart_overtemperature();
 static void slaves_uart_no_comms();
 static void slaves_uart_comms_error();
+static void cmd_creator();
+
+#define BMS_UART 0 //define which register to communicate with BMS
+#define BMS_TOTAL_TEMP_READINGS 10
+
+typedef struct {
+    uint8_t voltage;
+    uint8_t temperature[BMS_TOTAL_TEMP_READINGS];
+} BMSSlaveUART_t;
+
+static BMSSlaveUART_t *BMSSlave_normal;
+static BMSSlaveUART_t *BMSSlave_high;
+static BMSSlaveUART_t *BMSSlave_low;
+
+static void slaves_uart_init(){
+    // initialize data structures that will be used to set values 
+    BMSSlave_normal->voltage = 72;
+    for (int i=0; i<BMS_TOTAL_TEMP_READINGS; i++){
+        BMSSlave_normal->temperature[i] = 25;
+    }
+
+    BMSSlave_high->voltage = 120;
+    for (int i=0; i<BMS_TOTAL_TEMP_READINGS; i++){
+        BMSSlave_high->temperature[i] = 61;
+    }
+
+    BMSSlave_low->voltage = 34;
+    for (int i=0; i<BMS_TOTAL_TEMP_READINGS; i++){
+        BMSSlave_low->temperature[i] = 0;
+    }
+
+}
 
 Result_t bms_slaves_uart_process(uint8_t state){
 
@@ -49,29 +82,33 @@ Result_t bms_slaves_uart_process(uint8_t state){
 }
 
 static void normal_slaves_uart(){
-
+    // response message with all values in normal operating range
 }
 
 static void slaves_uart_undervoltage(){
-
+    // response message with a low voltage value(s), other data normal range
 }
 
 static void slaves_uart_overvoltage(){
-
+    // response message with high voltage value(s), other data normal range
 }
 
 static void slaves_uart_undertemperature(){
-
+    // response message with low temperature value(s), other data normal range
 }
 
 static void slaves_uart_overtemperature(){
-
+    // response message with high temperature value(s), other data normal range
 }
 
 static void slaves_uart_no_comms(){
-
+    // no response message - do nothing
 }
 
 static void slaves_uart_comms_error(){
-    
+    // response message with error in communication protocol - bad CRC, invalid message length, etc
+}
+
+static void cmd_creator(){
+    //  creates string of bits to be sent to the BMS
 }
